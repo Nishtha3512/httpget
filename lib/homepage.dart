8c3cb5682aaa5c63952a2ca57741e3bd4a16f073
Nishtha <nishthaarora002@gmail.com>
2020-07-09 09:37:42 +0530

    Changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Map data;
   List userData;
+  String _searchText = "";
   Future getData()async{
     http.Response response=await http.get("https://reqres.in/api/users?page=2");
     debugPrint(response.body);
@@ -23,16 +24,22 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState(){
     super.initState();
+    t1.addListener(() {
+
+      _printLatestValue();
+    });
     getData();
   }
 
   final TextEditingController t1=TextEditingController();
-
+  _printLatestValue(){
+    print("Text field:+$userData");
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: TextField(
+        /*title: TextField(
           controller: t1,
           decoration: InputDecoration
             (hintText: "Search here"),
@@ -40,21 +47,36 @@ class _HomePageState extends State<HomePage> {
         actions: [
           IconButton(
             icon: Icon(Icons.search, color: Colors.white,), onPressed: () {},)
-        ],
+        ],*/
+        title: Text("search.."),
       ),
-      body: ListView.builder(itemCount: userData == null ? 0: userData.length,
-      itemBuilder: (BuildContext context,int index){
-        return Card(
-          child: Column(
-            children: [
-              Text("${userData[index]["first_name"]}"),
-              //print("${userData[index]["first_name"]}");
-            ],
-          ),
-        );
-      }
-       ),
+      body: ListView.builder(//itemCount: userData == null ? 0: userData.length,
 
+      itemBuilder: (BuildContext context,int index){
+        return index==0 ? _searchBar() :
+          Card(
+            child: Column(
+              children: [
+                Text("${userData[index]["first_name"]}",style: TextStyle(fontSize: 20),),
+                Text("${userData[index]["last_name"]}",style: TextStyle(fontSize: 20),),
+                //print("${userData[index]["first_name"]}");
+              ],
+            ),
+
+        );
+      },
+        itemCount: userData.length,
+       ),
+    );
+  }
+  _searchBar(){
+    return TextField(
+      decoration: InputDecoration(
+        hintText: "Search....."
+      ),
+      onChanged: (text){
+
+      },
     );
   }
 }
